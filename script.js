181a138e7736c78d327bbe6ce4429556e9303ab6
Krystian Domżaowicz <krystian.domza@gmail.com>
2022-11-16 16:17:07 +0100

    feat(layout): add rest of layout(imag &  password section)

--- script.js
+++ script.js
@@ -28,21 +28,23 @@ const alphabet = [
 
 const passwords = ["krystian", "neoteric task", "mentoring"];
 
-let password =
+const password =
   passwords[Math.floor(Math.random() * passwords.length)].toLowerCase();
 let hiddenPassword = "";
 let defeatCounter = 0;
 
 // password hiding
-for (i = 0; i < password.length; i++) {
-  if (password.charAt(i) === " ") {
-    hiddenPassword = hiddenPassword + "";
-  } else {
-    hiddenPassword = hiddenPassword + "-";
+const hidePassword = () => {
+  for (i = 0; i < password.length; i++) {
+    if (password.charAt(i) === " ") {
+      hiddenPassword += " ";
+    } else {
+      hiddenPassword += "-";
+    }
   }
-}
+};
 
-//send hidden password to HTML
+//function to send hidden password to HTML
 const writePassword = () => {
   document.getElementById("password").innerHTML = hiddenPassword;
 };
@@ -54,17 +56,73 @@ const initialFunction = () => {
   alphabet.map((letter, i) => {
     return (listItems =
       listItems +
-      "<li class='letter' id=`letter-" +
+      "<li><button class='keyboard-letter' id=keyboard-letter-" +
       letter +
-      "` onclick='checkLetter(" +
+      " onclick=checkLetter('" +
       letter +
-      ")'>" +
+      "')>" +
       letter +
-      "</li>");
+      "</button></li>");
   });
 
   document.getElementById("keyboard").innerHTML = listItems;
+  document.getElementById("image-container").innerHTML =
+    '<img class="img-fluid" src="./images/s0.jpg" alt="wisielec"/>';
+  hidePassword();
   writePassword();
 };
 
 window.onload = initialFunction();
+
+const showLetter = (letter, index) => {
+  index.map((i) => {
+    return (hiddenPassword =
+      hiddenPassword.substring(0, i) +
+      letter +
+      hiddenPassword.substring(i + 1));
+  });
+  writePassword();
+  console.log(password === hiddenPassword);
+};
+
+const checkLetter = (letter) => {
+  const button = document.getElementById("keyboard-letter-" + letter + "");
+  let arrayOfIndexs = [];
+  let hit = false;
+
+  for (i = 0; i < password.length; i++) {
+    if (letter === password[i]) {
+      arrayOfIndexs.push(i);
+      hit = true;
+    }
+  }
+
+  if (hit) {
+    showLetter(letter, arrayOfIndexs);
+    button.classList.add("succes-button");
+  } else {
+    defeatCounter++;
+    button.classList.add("lost-button");
+  }
+
+  button.disabled = true;
+  button.classList.add("disabled");
+
+  document.getElementById("image-container").innerHTML =
+    '<img class="img-fluid" src="./images/s' +
+    defeatCounter +
+    '.jpg" alt="wisielec"/>';
+
+  //win
+  if (password === hiddenPassword) {
+    document.getElementById("modal-hook").innerHTML =
+      '<div id="modal"><div class="modal-content win">Nice! You guessed the password! <button class="reset-button" onclick="location.reload()" type="button">Reset</button></div></div>';
+  }
+  //lost
+  if (defeatCounter === 9) {
+    document.getElementById("modal-hook").innerHTML =
+      '<div id="modal"><div class="modal-content lost">You lost. The password was:<span style="color: #fc481c"> ' +
+      password +
+      '</span> <button class="reset-button" onclick="location.reload()" type="button">Reset</button></div></div>';
+  }
+};
